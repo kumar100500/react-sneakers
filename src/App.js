4e617aa7cce32b78b089483efa4bc048d563e01c
kumar100500 <moiyascik@gmail.com>
2023-03-15 14:32:59 +0200

    Вынес карточки в отдельный модуль

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
 
+const arr = [
+   {title:'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: '/img/sneakers/1.jpg'},
+   {title:'Мужские Кроссовки Nike Air Max 270', price: 15600, imageUrl: '/img/sneakers/2.jpg'},
+   {title:'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499, imageUrl: '/img/sneakers/3.jpg'},
+   {title:'Мужские Кроссовки Puma X Aka Boku Future Ride', price: 8999, imageUrl: '/img/sneakers/4.jpg'},
+]  
+
 function App() {
   return (
     <div className="wrapper clear">
       
       <Drawer/>   
       <Header/>
+      
      
       <div className="content p-40">
          <div className="d-flex align-center mb-40 justify-between"> 
@@ -20,8 +28,15 @@ function App() {
          
          <div className="d-flex">
 
-            <Card/>
-         
+            {arr.map((obj) =>(
+               <Card 
+               title= {obj.title}
+               price={obj.price}
+               imageUrl={obj.imageUrl} 
+            />
+            ))}
+
+            
          </div>
       </div>
     </div>
